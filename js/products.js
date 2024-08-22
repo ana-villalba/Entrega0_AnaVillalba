2e2ab972f7217e882a4aacc59cb79573d3101d5c
Gabriela Silveira <gabysole72@gmail.com>
2024-08-22 09:02:29 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,15 +1,13 @@
-let productsArray = [];
-
-// Función para mostrar la lista de productos
+let autos= `https://japceibal.github.io/emercado-api/cats_products/101.json`
 function showProductsList(array) {
     let htmlContentToAppend = "";
 
     for (let i = 0; i < array.length; i++) {
         let product = array[i];
         htmlContentToAppend += `
-        <div class="product-item">
+        <div class="product-item" >
             <div class="product-image">
-                <img src="` + product.image + `" alt="product image" class="img-thumbnail">
+                <img src=" `+ product.image +` " alt="product image" class="img-thumbnail">
             </div>
             <div class="product-details">
                 <p class="product-name">`+ product.name + `<p>
@@ -26,11 +24,23 @@ function showProductsList(array) {
 
 // Cargar los datos cuando el DOM esté listo
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
+    getJSONData(autos).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             productsArray = resultObj.data.products;
             showProductsList(productsArray);
         }
     });
+});
+
+
+function fetchProducts() {fetch(PRODUCTS_URL)
+.then(data => data.json()
+).then (data => {
+    productsArray = data;
+    showProductsList(productsArray);
+
+  } );} 
+  document.addEventListener("DOMContentLoaded", function() {
+    fetchProducts();
 });
