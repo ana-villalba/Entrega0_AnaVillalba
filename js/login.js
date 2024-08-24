6b3e95805b99f280b946a4313a823d1d1f312722
agostinapaiva <agospg7@gmail.com>
2024-08-24 16:08:36 -0300

    código viejo

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@
 function login(){
     //Desarrolla aquí la función del botón para la validación de datos y redirección a index.html
     // 1. Obtén los valores de los campos de entrada "username" y "password" por su ID.
-    let username= document.getElementById("username").value;
-    let password= document.getElementById("password").value;
+    let username= document.getElementById("username");
+    let password= document.getElementById("password");
     // 2. Verifica si ambos campos tienen valores. Puedes usar una condición if.
         if (username === ''|| password === '') {
             alert ('Completar todos los campos');
@@ -11,6 +11,6 @@ function login(){
             sessionStorage.setItem("session", "loggedin");
             localStorage.setItem("userlogger", username);
             localStorage.setItem("userpassword", password);
-            window.location.href = "/js/index.html";
+            window.location.href = "index.html";
         }
     }
