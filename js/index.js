e6a7b53455061b462658ab6b017d916ad6c162c4
agostinapaiva <agospg7@gmail.com>
2024-08-24 16:22:23 -0300

    cambiosssss

--- js/index.js
+++ js/index.js
@@ -14,12 +14,7 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Verifica si el usuario está logueado
-    if (!sessionStorage.getItem("loggedIn")) {
-        // Muestra una alerta indicando que debe loguearse
-        alert("Por favor, inicia sesión para acceder a esta página");
-
-        // Redirige a login.html
-        window.location.href = "login.html";
-    }
-});
+    if (!sessionStorage.getItem("sesion")) {
+            window.location.href = "login.html";
+        } 
+    });
